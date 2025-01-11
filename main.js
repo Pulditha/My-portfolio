75f0645d148c963bcb8ff6e1be5d984924a00158
Pulditha <cb011498@students.apiit.lk>
2025-01-11 22:27:54 +0530

    loading page

--- main.js
+++ main.js
@@ -66,52 +66,55 @@ window.addEventListener("resize", () => {
   ScrollTrigger.refresh();
 });
 
-// // Loader functionality
-// function startLoader() {
-//   let counterElement = document.querySelector(".counter");
-//   let currentValue = 0;
 
-//   function updateCounter() {
-//     if (currentValue === 100) {
-//       return;
-//     }
-//     currentValue += Math.floor(Math.random() * 10) + 1;
 
-//     if (currentValue > 100) {
-//       currentValue = 100;
-//     }
-//     counterElement.textContent = currentValue;
 
-//     let delay = Math.floor(Math.random() * 200) + 50;
-//     setTimeout(updateCounter, delay);
-//   }
+// Loader functionality
+function startLoader() {
+  let counterElement = document.querySelector(".counter");
+  let currentValue = 0;
 
-//   updateCounter();
-// }
+  function updateCounter() {
+    if (currentValue === 100) {
+      return;
+    }
+    currentValue += Math.floor(Math.random() * 10) + 1;
 
-// startLoader();
+    if (currentValue > 100) {
+      currentValue = 100;
+    }
+    counterElement.textContent = currentValue;
 
-// gsap.to(".counter", 0.25, {
-//   delay: 3.5,
-//   opacity: 0,
-// });
-// gsap.to(".bar", 1.5, {
-//   delay: 3.5,
-//   height: 0,
-//   stagger: {
-//     amount: 0.5,
-//   },
-//   ease: "power4.inOut",
-// });
+    let delay = Math.floor(Math.random() * 200) + 50;
+    setTimeout(updateCounter, delay);
+  }
 
-// gsap.from(".h1", 1.5, {
-//   delay: 4,
-//   y: 700,
-//   stagger: {
-//     amount: 0.5,
-//   },
-//   ease: "power4.inOut",
-// });
+  updateCounter();
+}
+
+startLoader();
+
+gsap.to(".counter", 0.25, {
+  delay: 3.5,
+  opacity: 0,
+});
+gsap.to(".bar", 1.5, {
+  delay: 3.5,
+  height: 0,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+});
+
+gsap.from(".h1", 1.5, {
+  delay: 4,
+  y: 700,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+});
 
   // Smooth scroll for anchor links
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
